9da9fe6529bc348f797ea11af1c66be92b7fc647
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-09-15 02:45:15 -0700

    serializers

---
+++ core/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from core.models import *
+
+class CustomUserManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUserManager
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class ChildrenSectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChildrenSection
+        fields = '__all__'
+
+class TrainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trainer
+        fields = '__all__'
