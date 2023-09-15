e37b4a564b547b3fc4ca601ab824b9d76e77b232
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-09-15 02:31:18 -0700

    models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email')
+
+@admin.register(ProfileKid)
+class ProfileKidAdmin(admin.ModelAdmin):
+    list_display = ('id', 'kid_name', 'profile')
+
+@admin.register(ChildrenSection)
+class ChildrenSectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_club', 'description', 'club_kinds', 'record')
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ('trainer_name', 'trainer_last_name')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'children_section')
--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
-from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -27,8 +27,8 @@ class CustomUserManager(BaseUserManager):
 class Profile(AbstractBaseUser):
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField("email address", unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -47,6 +47,37 @@ class Profile(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
-    @property
-    def is_staff(self):
-        return self.is_superuser
+class ProfileKid(models.Model):
+    kid_name = models.CharField(max_length=100, verbose_name='Имя ребенка')
+    profile = models.ForeignKey(Profile, related_name='дети', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.kid_name} (родитель: {self.profile})'
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    children_section = models.ForeignKey('ChildrenSection', on_delete=models.CASCADE)
+
+class ChildrenSection(models.Model):
+    clubs_kinds_CHOICES = [
+        ('развитие', 'Развитие'),
+        ('спорт', 'Спорт'),
+        ('искусство', 'Искусство'),
+    ]
+    rating = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(1, 6)])
+    name_club = models.CharField(max_length=100)
+    trainer = models.ManyToManyField('Trainer', related_name='секции')
+    description = models.TextField()
+    club_kinds = models.CharField(max_length=20, choices=clubs_kinds_CHOICES)
+    record = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name_club
+
+class Trainer(models.Model):
+    trainer_name = models.CharField(max_length=100)
+    trainer_last_name = models.CharField(max_length=100)
+    trainer_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.trainer_name} {self.trainer_last_name}'
